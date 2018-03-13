c3911b3337d05f8e488f8d9ea6eb3f84340f5f52
zhangxiaochen <zhangxiaochen@zenmen.com>
2018-03-13 18:40:58 +0800

    base line notebook

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ Defines various functions for processing the data.
 import numpy as np
 import soundfile
 from numpy.lib.stride_tricks import as_strided
-from char_map import char_map, index_map
+
 import librosa
 
 def calc_feat_dim(window, max_freq):
